0377243bf0272729dd3b3b9bf065bc72cc5038fa
ds5678 <49847914+ds5678@users.noreply.github.com>
2024-06-09 20:09:47 -0700

    Update with LocalAABB

--- configuration.sh
+++ configuration.sh
@@ -4,5 +4,5 @@ path_to_7zip="/mnt/c/Program Files/7-Zip/7z.exe"
 path_to_UnitySetup="/mnt/f/UnityInstallers"
 path_to_UnityInstallations="/mnt/g/TypeTreeDumps/UnityInstallations"
 
-path_to_EngineFileExtractor="/mnt/e/repos/AssetRipper.Mining/AssetRipper.Mining.EngineFileExtractor/bin/Release/net7.0/AssetRipper.Mining.EngineFileExtractor.exe"
-path_to_EngineAssetsJson="/mnt/e/repos/AssetRipper.Mining/AssetRipper.Mining.EngineFileExtractor/bin/Release/net7.0/engineassets.json"
+path_to_EngineFileExtractor="/mnt/e/repos/AssetRipper.Mining/AssetRipper.Mining.EngineFileExtractor/bin/Release/net8.0/AssetRipper.Mining.EngineFileExtractor.exe"
+path_to_EngineAssetsJson="/mnt/e/repos/AssetRipper.Mining/AssetRipper.Mining.EngineFileExtractor/bin/Release/net8.0/engineassets.json"
